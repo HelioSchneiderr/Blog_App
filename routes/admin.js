c1e07ad624d5c72b5c2935c2ad4f8bfb98df8c96
HelioSchneiderr <helioschndr@gmail.com>
2022-07-06 18:18:54 -0300

    BlogApp - V5

--- routes/admin.js
+++ routes/admin.js
@@ -17,9 +17,14 @@ router.get("/posts", (req, res) =>{
     res.send("Page for posts")
 })
 
-router.get("/categories", (req, res)=>{
-    res.render(`admin/categories`)
-});
+router.get('/categories', (req, res) => {
+    Categoria.find().lean().then((categories) => {
+        res.render('admin/categories', {categories: categories})
+    }).catch((err) => {
+        req.flash('error_msg', 'Erro ao listar categorias')
+        res.redirect('/admin')
+    })
+})
 
 router.get(`/categories/add`, (req, res) => {
     res.render("admin/addcategories")
@@ -41,18 +46,26 @@ router.post("/categories/nova", (req, res) =>{
         erros.push({texto: "Nome da Categoria muito pequeno"})
     }
     
+    if(erros.length > 0){
+        res.render("admin/addcategories", {erros: erros })
+    }else{
+        const newCategorie = {
+            nome: req.body.nome,
+            slug: req.body.slug
+        };
+    
+        new Categoria(newCategorie).save().then(() => {
+            req.flash("success_msg", "Categoria Registrada com sucesso")
+            res.redirect("/admin/categories")
+        }).catch((err) =>{
+            req.flash("error_msg", "Houve um erro ao salvar a mensagem")
+            res.redirect("/admin")
+        })
+
+    }
     
 
-    const newCategorie = {
-        nome: req.body.nome,
-        slug: req.body.slug
-    };
-
-    new Categoria(newCategorie).save().then(() => {
-        console.log("Categoria salva com sucesso!")
-    }).catch((err) =>{
-        console.log("Erro ao salvar" + err)
-    })
+    
 });
 
 module.exports = router;
