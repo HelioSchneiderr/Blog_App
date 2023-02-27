eb0ae9a86113343c8e9fc1d00e059eb057e80455
Hélio Schneider <helioschndr@gmail.com>
2023-02-27 14:02:48 -0300

    BlogApp - V13

--- app.js
+++ app.js
@@ -54,7 +54,7 @@
 
     //Mongoose
         mongoose.Promise = global.Promise;
-        mongoose.connect("mongodb://localhost/blogapp").then(() =>{
+        mongoose.connect("mongodb://127.0.0.1:27017/blogapp").then(() =>{
             console.log("Conectado ao mongo")
         }).catch((err) => {
             console.log("Erro ao se conectar: " +err)
--- routes/usuario.js
+++ routes/usuario.js
@@ -86,10 +86,10 @@ router.post("/login", (req, res, next) =>{
     })(req, res, next)
 })
 
-router.get("/logout", (req, res) => {
-    req.logout()
-    req.flash("success_msg", "Deslogado com sucesso!")
-    redirect("/")
+router.get('/logout', (req, res, next) => {
+    req.logout(function(err) {
+        if (err) { return next(err) }
+        res.redirect('/')
+      })
 })
-
 module.exports = router;
